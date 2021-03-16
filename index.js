d8ebfcf53be7a02b9d0b8dd4925c86bdc3272b5f
Xiaochen Xu <xiaochex@gmail.com>
2021-03-15 23:17:29 -0400

    commit tree a12ac385175d7c20d843d51de3a6526cfd057a22

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+multiply
+
+
+let random = Math.floor(Math.random()) + 1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4
+mod
+
+
+let max = Math.max(1,2,3,15,20);
